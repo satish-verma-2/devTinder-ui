dad01fe8853340ab16e861b9748a484b8b10eb6a
satish <satish@greymedia.co>
2026-09-09 15:35:55 +0530

    redux setup and login api integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
-import { BrowserRouter, Route, Routes } from "react-router"
-import Body from "./Body"
-import Login from "./Login"
-import Profile from "./Profile"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Feed from "./components/Feed";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-
-
-          <Route path="/" element={<Body />} >
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-
-
-
-
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
 
-
-
-{/* both syntax are correct but we do write props in opening tag <ROute > and we acutally make it
+{
+  /* both syntax are correct but we do write props in opening tag <ROute > and we acutally make it
           self clossing tag like <Route path="" element={} but we when we need to show children of this then 
-          have to use <Route path="" element={}>  </Route>*/}
+          have to use <Route path="" element={}>  </Route>*/
+}
 
-{/* <Route path="/profile" element={<div>Profile Page</div>} />
-          <Route path="/signup" element={<div>Signup Page</div>}></Route> */}
+{
+  /* <Route path="/profile" element={<div>Profile Page</div>} />
+          <Route path="/signup" element={<div>Signup Page</div>}></Route> */
+}
---
+++ src/components/Login.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+         BASE_URL + "/login",
+        { email, password },
+        { withCredentials: true },
+      );
+      console.log("res.....", res.data);
+      dispatch(addUser(res.data));
+      return navigate("/");
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  };
+  return (
+    <div className="flex justify-center">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input
+                type="text"
+                className="input"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="password"
+                className="input"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
